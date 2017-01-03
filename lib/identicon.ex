9cfb4444c4bbb08f10abdfa92eb7c49d9c5f3948
matze <matthiaslemmer@aol.de>
2017-01-03 17:42:17 +0100

    implement hash

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,24 @@
 defmodule Identicon do
+
+    def main(input) do
+        input 
+        |> hash_input()   
+    end
+
+    @doc
+    """
+
+    """
+    defp hash_input(input) do
+        :crypto.hash(:md5, input) 
+        |> :binary.bin_to_list()   
+    end
+
+    @doc
+    """
+
+    """
+    defp do_sthg(username) do
+        
+    end
 end
