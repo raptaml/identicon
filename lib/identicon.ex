d634ddcdb77b41ba3eca8c516ef8b2675db23b60
Matthias Lemmer <lemmer@kks.local>
2017-01-04 15:38:53 +0100

    color, grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,14 +3,15 @@ defmodule Identicon do
     def main(input) do
         input 
         |> hash_input()
-        |> set_color()   
+        |> set_color()
+        |> build_grid   
     end
 
     @doc
     """
 
     """
-    defp hash_input(input) do
+    def hash_input(input) do
         hex = :crypto.hash(:md5, input) 
         |> :binary.bin_to_list()   
 
@@ -21,9 +22,28 @@ defmodule Identicon do
     """
 
     """
-    defp set_color(image) do
-        color = Enum.chunk(image.hex, 3)
-        image = 
+    def set_color(image) do
+        %Identicon.Image{hex: hex_list} = image
+        [r,g,b | _tail] = hex_list
+        %Identicon.Image{image | color: {r,g,b}}
+
         
+    end
+
+    def build_grid(image) do
+        grid =
+            image.hex
+            |> Enum.chunk(3)
+            |> Enum.map(&mirror_row/1)
+            |> List.flatten
+            |> Enum.with_index
+            
+        %Identicon.Image{image | grid: grid }
+    end
+
+    defp mirror_row(row) do
+        #[a,b | _tail] =  row
+        first = Enum.slice(row,0,2)
+        row ++ Enum.reverse(first)       
     end
 end
