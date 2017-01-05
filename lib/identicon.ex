ae4854df9acd010712ff9ca10a46e9bae59f53bd
Matthias Lemmer <lemmer@kks.local>
2017-01-05 16:30:17 +0100

    implement build_pixel_map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,7 +4,9 @@ defmodule Identicon do
         input 
         |> hash_input()
         |> set_color()
-        |> build_grid   
+        |> build_grid
+        |> filter_odd
+        |> build_pixel_map   
     end
 
     @doc
@@ -37,8 +39,25 @@ defmodule Identicon do
             |> Enum.map(&mirror_row/1)
             |> List.flatten
             |> Enum.with_index
-            
+
         %Identicon.Image{image | grid: grid }
+    end
+
+    def filter_odd(image) do
+        grid = Enum.filter(image.grid, fn({number, _index}) ->
+            rem(number,2) == 0 
+        end)
+        %Identicon.Image{image | grid: grid}
+    end
+
+    def build_pixel_map(image) do
+        grid = image.grid
+        step = 50
+        pixelmap = Enum.map(grid, fn({_number, index}) ->
+            {{x1,x2},{y1,y2}} = {{rem(index, 5) * 50, div(index, 5) * 50}, {rem(index, 5) * 50 + 50, div(index, 5) * 50 + 50  }}
+        end)
+        %Identicon.Image{image | pixel_map: pixelmap}
+ 
     end
 
     defp mirror_row(row) do
